a354fadd1c2847965f051e5bc98a2743b175e7b8
Stephen Wilson <stephenwilson1984@outlook.com>
2026-05-06 16:37:08 +0100

    Checkpoint

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,14 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  let tabContent = 'Please click a button.';
+  const [selectedTopic, setSelectedTopic] = useState('components');
   
-  function handleOnSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    tabContent = selectedButton;
+  function handleOnSelect(selectedButtonText) {
+    setSelectedTopic(selectedButtonText);
   }
 
   return (
@@ -30,12 +30,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleOnSelect("Components")}>Components</TabButton>
-            <TabButton onSelect={() => handleOnSelect("JSX")}>JSX</TabButton>
-            <TabButton onSelect={() => handleOnSelect("Props")}>Props</TabButton>
-            <TabButton onSelect={() => handleOnSelect("State")}>State</TabButton>
+            <TabButton onSelect={() => handleOnSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleOnSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleOnSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleOnSelect("state")}>State</TabButton>
           </menu>
-          { tabContent }
+          <div id="tab-content">
+            <h3>{ EXAMPLES[selectedTopic].title }</h3>
+            <p>{ EXAMPLES[selectedTopic].description }</p>
+            <pre>
+              <code>
+                { EXAMPLES[selectedTopic].code }
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
